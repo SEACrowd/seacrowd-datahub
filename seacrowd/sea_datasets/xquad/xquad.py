593e208cda3cecf1acc33af039eb091ac6536b14
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2023-11-19 23:08:31 +0000

    Fix test by using count as id, because otherwise the id might duplicate between language

--- seacrowd/sea_datasets/xquad/xquad.py
+++ seacrowd/sea_datasets/xquad/xquad.py
@@ -1,5 +1,4 @@
 import json
-import os
 from pathlib import Path
 from typing import Dict, List, Tuple
 
@@ -7,7 +6,7 @@ import datasets
 
 from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
-from seacrowd.utils.constants import Tasks, Licenses
+from seacrowd.utils.constants import Licenses, Tasks
 
 _CITATION = """\
 @article{Artetxe:etal:2019,
@@ -24,7 +23,9 @@ _CITATION = """\
 _DATASETNAME = "xquad"
 
 _DESCRIPTION = """\
-XQuAD (Cross-lingual Question Answering Dataset) is a benchmark dataset for evaluating cross-lingual question answering performance. The dataset consists of a subset of 240 paragraphs and 1190 question-answer pairs from the development set of SQuAD v1.1 (Rajpurkar et al., 2016) together with their professional translations into ten languages: Spanish, German, Greek, Russian, Turkish, Arabic, Vietnamese, Thai, Chinese, and Hindi. Consequently, the dataset is entirely parallel across 11 languages.
+XQuAD (Cross-lingual Question Answering Dataset) is a benchmark dataset for evaluating cross-lingual question answering performance.
+The dataset consists of a subset of 240 paragraphs and 1190 question-answer pairs from the development set of SQuAD v1.1 together
+with their professional translations into ten languages: Spanish, German, Greek, Russian, Turkish, Arabic, Vietnamese, Thai, Chinese, and Hindi.
 """
 
 _HOMEPAGE = "https://github.com/google-deepmind/xquad"
@@ -49,7 +50,7 @@ class XQuADDataset(datasets.GeneratorBasedBuilder):
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     SEACROWD_VERSION = datasets.Version(_SEACROWD_VERSION)
 
-    subsets = ["xquad.vi", "xquad.th"]
+    subsets = ["xquad", "xquad.vi", "xquad.th"]
 
     BUILDER_CONFIGS = [
         SEACrowdConfig(
@@ -71,7 +72,7 @@ class XQuADDataset(datasets.GeneratorBasedBuilder):
         for subset in subsets
     ]
 
-    DEFAULT_CONFIG_NAME = "xquad.vi_source"
+    DEFAULT_CONFIG_NAME = "xquad_source"
 
     def _info(self) -> datasets.DatasetInfo:
         if self.config.schema == "source":
@@ -91,45 +92,65 @@ class XQuADDataset(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
         name_split = self.config.name.split("_")
-        filepath = dl_manager.download_and_extract(_URLS + name_split[0] + ".json")
+        subset_name = name_split[0]
+
+        if subset_name == "xquad":
+            # For 'xquad' subset, download and combine Vietnamese and Thai subsets
+            vi_filepath = dl_manager.download_and_extract(_URLS + "xquad.vi.json")
+            th_filepath = dl_manager.download_and_extract(_URLS + "xquad.th.json")
+            filepaths = [vi_filepath, th_filepath]
+        else:
+            # For specific language subsets
+            filepath = dl_manager.download_and_extract(_URLS + subset_name + ".json")
+            filepaths = [filepath]
 
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 gen_kwargs={
-                    "filepath": filepath,
+                    "filepaths": filepaths,
                     "split": "train",
                 },
             )
         ]
 
-    def _generate_examples(self, filepath: Path, split: str) -> Tuple[int, Dict]:
-        with open(filepath, encoding="utf-8") as f:
-            data = json.load(f)
-
+    def _generate_examples(self, filepaths: List[Path], split: str) -> Tuple[int, Dict]:
         count = 0
-        for paragraphs in data["data"]:
-            for example in paragraphs["paragraphs"]:
-                context = example["context"]
-                for qa in example["qas"]:
-                    question = qa["question"]
-                    id_ = qa["id"]
-                    answers = qa["answers"]
-                    answers_start = [answer["answer_start"] for answer in answers]
-                    answers_text = [answer["text"] for answer in answers]
+        document_count = 0
+        for filepath in filepaths:
+            with open(filepath, encoding="utf-8") as f:
+                data = json.load(f)
 
-                    if self.config.schema == "source":
-                        yield count, {
-                            "context": context,
-                            "question": question,
-                            "answers": {"answer_start": answers_start, "text": answers_text},
-                            "id": id_,
-                        }
-                        count += 1
+            for paragraphs in data["data"]:
+                for example in paragraphs["paragraphs"]:
+                    context = example["context"]
+                    for qa in example["qas"]:
+                        question = qa["question"]
+                        answers = qa["answers"]
+                        answers_start = [answer["answer_start"] for answer in answers]
+                        answers_text = [answer["text"] for answer in answers]
 
-                    elif self.config.schema == "seacrowd_qa":
-                        yield count, {"question_id": id_, "context": context, "question": question, "answer": {"answer_start": answers_start[0], "text": answers_text[0]}, "id": id_, "choices": [], "type": "extractive", "document_id": count}
+                        if self.config.schema == "source":
+                            yield count, {
+                                "context": context,
+                                "question": question,
+                                "answers": {"answer_start": answers_start, "text": answers_text},
+                                "id": count,
+                            }
+                        elif self.config.schema == "seacrowd_qa":
+                            yield count, {
+                                "question_id": count,
+                                "context": context,
+                                "question": question,
+                                "answer": {"answer_start": answers_start[0], "text": answers_text[0]},
+                                "id": count,
+                                "choices": [],
+                                "type": "extractive",
+                                "document_id": document_count,
+                            }
+
                         count += 1
+                    document_count += 1
 
 
 if __name__ == "__main__":
