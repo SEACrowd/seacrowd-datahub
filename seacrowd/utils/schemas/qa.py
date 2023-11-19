38d67850fede863f2fa3cee6f46229fece528c1e
sabilmakbar <maulana.1998@yahoo.co.id>
2023-11-19 23:29:40 +0700

    Add comments on how to use 'meta' field

--- seacrowd/utils/schemas/qa.py
+++ seacrowd/utils/schemas/qa.py
@@ -13,8 +13,14 @@ features = datasets.Features(
         "choices": datasets.Sequence(datasets.Value("string")),
         "context": datasets.Value("string"),
         "answer": datasets.Sequence(datasets.Value("string")),
-        # fill meta field in `_generate_examples` as empty dict if no info can be added in here
-        # the schema aren't specified either to allow some flexibility
+
+        # the schema of 'meta' aren't specified either to allow some flexibility
         "meta": {}
+
+        # notes on how to use this field of 'meta'
+        # you can choose two of options:
+        # 1. defining as empty dict if you don't think it's usable in `_generate_examples`, or
+        # 2. defining meta as dict of key with intended colname meta and its val with dataset.Features class
+        #    in `_info` Dataloader method then populate it with the values in `_general_examples` Dataloader method
     }
 )
