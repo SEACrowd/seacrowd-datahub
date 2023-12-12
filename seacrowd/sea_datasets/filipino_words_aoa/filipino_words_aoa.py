b2d7de139a8bf7aee278d5319696c6e376a7e759
elyanah <aco.elyanah@gmail.com>
2023-12-12 08:16:42 +0800

    Prepare dataloader for PR; add openpyxl in requirements

--- seacrowd/sea_datasets/filipino_words_aoa/filipino_words_aoa.py
+++ seacrowd/sea_datasets/filipino_words_aoa/filipino_words_aoa.py
@@ -129,9 +129,3 @@ class FilipinoWordsAOADataset(datasets.GeneratorBasedBuilder):
                     "text_2_name": "eng",
                 }
             yield index, example
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
