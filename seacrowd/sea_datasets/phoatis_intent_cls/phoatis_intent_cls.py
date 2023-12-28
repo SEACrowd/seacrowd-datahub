422194521d8ae097637c59ebcf191b23b78dd661
Muhammad Dehan Al Kautsar <dehanalkautsar3@gmail.com>
2023-12-28 13:21:59 +0700

    Revert "remove __name__ = __main__ in PhoATIS intent cls"
    This reverts commit 32b175b5930f9c14d7f318fcf6e2f33411617467.

--- seacrowd/sea_datasets/phoatis_intent_cls/phoatis_intent_cls.py
+++ seacrowd/sea_datasets/phoatis_intent_cls/phoatis_intent_cls.py
@@ -1,16 +1,18 @@
+import os
 from pathlib import Path
 from typing import Dict, List, Tuple
 
 import datasets
+import pandas as pd
 
 from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
-from seacrowd.utils.constants import Tasks
+from seacrowd.utils.constants import Tasks, Licenses
 
 # TODO: Add BibTeX citation
 _CITATION = """\
 @article{dao2021intent,
-      title={Intent Detection and Slot Filling for Vietnamese},
+      title={Intent Detection and Slot Filling for Vietnamese}, 
       author={Mai Hoang Dao and Thinh Hung Truong and Dat Quoc Nguyen},
       year={2021},
       eprint={2104.02021},
@@ -32,39 +34,15 @@ _LICENSE = "Licenses.UNKNOWN.value"
 _URLS = {
     _DATASETNAME: {
         "syllable": {
-            "syllable_train": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/label",
-            ],
-            "syllable_dev": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/label",
-            ],
-            "syllable_test": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/label",
-            ],
+            "syllable_train": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/train/label"],
+            "syllable_dev": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/dev/label"],
+            "syllable_test": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/syllable-level/test/label"],
         },
         "word": {
-            "word_train": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/label",
-            ],
-            "word_dev": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/label",
-            ],
-            "word_test": [
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/seq.in",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/seq.out",
-                "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/label",
-            ],
-        },
+            "word_train": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/train/label"],
+            "word_dev": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/dev/label"],
+            "word_test": ["https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/seq.in", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/seq.out", "https://raw.githubusercontent.com/VinAIResearch/JointIDSF/main/PhoATIS/word-level/test/label"],
+        }
     }
 }
 
@@ -74,9 +52,8 @@ _SOURCE_VERSION = "1.0.0"
 
 _SEACROWD_VERSION = "1.0.0"
 
-
-def config_constructor(schema: str, version: str, phoatis_subset: str = "syllable") -> SEACrowdConfig:
-    assert phoatis_subset == "syllable" or phoatis_subset == "word"
+def config_constructor(schema: str, version: str, phoatis_subset: str = 'syllable') -> SEACrowdConfig:
+    assert phoatis_subset == 'syllable' or phoatis_subset == 'word'
 
     return SEACrowdConfig(
         name="phoatis_intent_cls_{phoatis_subset}_{schema}".format(phoatis_subset=phoatis_subset.lower(), schema=schema),
@@ -93,27 +70,24 @@ class PhoATIS(datasets.GeneratorBasedBuilder):
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     SEACROWD_VERSION = datasets.Version(_SEACROWD_VERSION)
 
-    BUILDER_CONFIGS = [config_constructor("source", _SOURCE_VERSION, schema) for schema in ["syllable", "word"]]
-    BUILDER_CONFIGS.extend([config_constructor("seacrowd_text", _SOURCE_VERSION, schema) for schema in ["syllable", "word"]])
+    BUILDER_CONFIGS = [config_constructor('source', _SOURCE_VERSION, schema) for schema in ['syllable', 'word']]
+    BUILDER_CONFIGS.extend([config_constructor('seacrowd_text', _SOURCE_VERSION, schema) for schema in ['syllable', 'word']])
 
-    BUILDER_CONFIGS.extend(
-        [  # Default config
-            SEACrowdConfig(
-                name="phoatis_intent_cls_source",
-                version=SOURCE_VERSION,
-                description="PhoATIS Intent Classification source schema (Syllable version)",
-                schema="source",
-                subset_id="syllable",
-            ),
-            SEACrowdConfig(
-                name="phoatis_intent_cls_seacrowd_text",
-                version=SEACROWD_VERSION,
-                description="PhoATIS Intent Classification SEACrowd schema (Syllable version)",
-                schema="seacrowd_text",
-                subset_id="syllable",
-            ),
-        ]
-    )
+    BUILDER_CONFIGS.extend([ #Default config
+        SEACrowdConfig(
+            name="phoatis_intent_cls_source",
+            version=SOURCE_VERSION,
+            description="PhoATIS Intent Classification source schema (Syllable version)",
+            schema="source",
+            subset_id="syllable",
+        ),
+        SEACrowdConfig(
+            name="phoatis_intent_cls_seacrowd_text",
+            version=SEACROWD_VERSION,
+            description="PhoATIS Intent Classification SEACrowd schema (Syllable version)",
+            schema="seacrowd_text",
+            subset_id="syllable",
+        ),])
 
     DEFAULT_CONFIG_NAME = "phoatis_intent_cls_source"
 
@@ -130,9 +104,9 @@ class PhoATIS(datasets.GeneratorBasedBuilder):
             )
 
         elif self.config.schema == "seacrowd_text":
-            with open(".\seacrowd\sea_datasets\phoatis_intent_cls\intent_label.txt", "r+", encoding="utf8") as fw:
+            with open('.\seacrowd\sea_datasets\phoatis_intent_cls\intent_label.txt', "r+", encoding="utf8") as fw:
                 intent_label = fw.read()
-                intent_label = intent_label.split("\n")
+                intent_label = intent_label.split('\n')
             features = schemas.text_features(intent_label)
 
         return datasets.DatasetInfo(
@@ -175,13 +149,13 @@ class PhoATIS(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, filepath: Path, split: str) -> Tuple[int, Dict]:
         with open(filepath[0], "r+", encoding="utf8") as fw:
             data_input = fw.read()
-            data_input = data_input.split("\n")
+            data_input = data_input.split('\n')
         with open(filepath[1], "r+", encoding="utf8") as fw:
             data_slot = fw.read()
-            data_slot = data_slot.split("\n")
+            data_slot = data_slot.split('\n')
         with open(filepath[2], "r+", encoding="utf8") as fw:
             data_intent = fw.read()
-            data_intent = data_intent.split("\n")
+            data_intent = data_intent.split('\n')
 
         if self.config.schema == "source":
             for idx, text in enumerate(data_input):
@@ -200,3 +174,9 @@ class PhoATIS(datasets.GeneratorBasedBuilder):
                 example["text"] = text
                 example["label"] = data_intent[idx]
                 yield example["id"], example
+
+
+# This allows you to run your dataloader with `python [dataset_name].py` during development
+# TODO: Remove this before making your PR
+if __name__ == "__main__":
+    datasets.load_dataset(__file__)
