e1208397438c3230ad97d49367ab3f3ce27012ce
Muhammad Dehan Al Kautsar <dehanalkautsar3@gmail.com>
2023-12-29 14:49:06 +0700

    remove __name__:__main__ on indocamrest

--- seacrowd/sea_datasets/indocamrest/indocamrest.py
+++ seacrowd/sea_datasets/indocamrest/indocamrest.py
@@ -161,9 +161,3 @@ class IndoCamRest(datasets.GeneratorBasedBuilder):
                     dialogue.append(dial)
                 example["dialogue"] = dialogue
                 yield str(idx), example
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
