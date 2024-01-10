4b33f58657960c8becd9bdbbcb1a5730342dc67b
Yuze GAO <yuze.gao@outlook.com>
2024-01-11 02:00:04 +0800

    Update id_vaccines_tweet.py

--- seacrowd/sea_datasets/id_vaccines_tweet/id_vaccines_tweet.py
+++ seacrowd/sea_datasets/id_vaccines_tweet/id_vaccines_tweet.py
@@ -4,9 +4,9 @@ import pandas
 from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
 from seacrowd.utils.constants import (DEFAULT_SEACROWD_VIEW_NAME,
-                                      DEFAULT_SOURCE_VIEW_NAME, Tasks)
+                                      DEFAULT_SOURCE_VIEW_NAME, Tasks, Licenses)
 
-_DATASETNAME = "id_vaccines"
+_DATASETNAME = "id_vaccines_tweets"
 _SOURCE_VIEW_NAME = DEFAULT_SOURCE_VIEW_NAME
 _UNIFIED_VIEW_NAME = DEFAULT_SEACROWD_VIEW_NAME
 
@@ -41,37 +41,37 @@ subjective tweet and their sentiment polarity. Tweets are from 20-27 June 2021 a
 
 _HOMEPAGE = "	https://github.com/rayendito/id-vaccines-tweets"
 
-_LICENSE = "UNKNOWN"
+_LICENSE = Licenses.UNKNOWN.value
 
 _URL = "https://raw.githubusercontent.com/rayendito/id-vaccines-tweets/main/id_vaccines_tweets.csv"
 
 _SUPPORTED_TASKS = [Tasks.SENTIMENT_ANALYSIS]
 _SOURCE_VERSION = "1.0.0"
-_SEACROWD_VERSION = "0.0.1"
+_SEACROWD_VERSION = "1.0.0"
 
 
-class id_vaccines_tweetDataset(datasets.GeneratorBasedBuilder):
-    """Dataset containing tweets about COVID-19 vaccines with manually labelled information about whether they are
-    a subjective tweet and their sentiment polarity. Tweets are from 20-27 June 2021 and 15-22 July 2021."""
+class IdVaccinesTweetsDataset(datasets.GeneratorBasedBuilder):
+    """This is a seacrowd dataloader for id_vaccines_tweets, for every example in the dataset, it contains a subjective
+    tweet and their sentiment polarity.  Tweets are from 20-27 June 2021 and 15-22 July 2021."""
 
     BUILDER_CONFIGS = [
         SEACrowdConfig(
-            name="id_vaccines_tweet_source",
+            name=f"{_DATASETNAME}_source",
             version=datasets.Version(_SOURCE_VERSION),
-            description="Dataset containing tweets about COVID-19 vaccines with manually labelled information about " "whether they are a subjective tweet and their sentiment polarity. " "Tweets are from 20-27 June 2021 and 15-22 July 2021.",
+            description=_DESCRIPTION,
             schema="source",
-            subset_id="id_vaccines_tweet",
+            subset_id=f"{_DATASETNAME}",
         ),
         SEACrowdConfig(
-            name="id_vaccines_tweet_seacrowd_text",
-            version=datasets.Version(_SOURCE_VERSION),
-            description="Dataset containing tweets about COVID-19 vaccines with manually labelled information about " "whether they are a subjective tweet and their sentiment polarity. " "Tweets are from 20-27 June 2021 and 15-22 July 2021.",
-            schema="seacrowd.text",
-            subset_id="id_vaccines_tweet",
+            name=f"{_DATASETNAME}_seacrowd_text",
+            version=datasets.Version(_SEACROWD_VERSION),
+            description=_DESCRIPTION,
+            schema="seacrowd_text",
+            subset_id=f"{_DATASETNAME}",
         ),
     ]
 
-    DEFAULT_CONFIG_NAME = "id_vaccines_tweet_source"
+    DEFAULT_CONFIG_NAME = f"{_DATASETNAME}_source"
 
     def _info(self):
         if self.config.schema == "source":
@@ -84,7 +84,7 @@ class id_vaccines_tweetDataset(datasets.GeneratorBasedBuilder):
                     "sentiment": datasets.Value("float"),
                 }
             )
-        elif self.config.schema == "seacrowd.text":
+        elif self.config.schema == "seacrowd_text":
             features = schemas.text_features([-1.0, 0.0, 1.0])
         else:
             raise ValueError(f"Invalid config: {self.config.name}")
@@ -100,9 +100,6 @@ class id_vaccines_tweetDataset(datasets.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager):
         """ "return splitGenerators"""
         downloaded_files = dl_manager.download_and_extract(_URL)
-        print(f"Saved to {downloaded_files}")
-        print(f"This dataset is {self.config.name.replace('_source', '')}")
-
         return [datasets.SplitGenerator(name="datasets.ALL", gen_kwargs={"filepath": downloaded_files})]
 
     def _generate_examples(self, filepath):
@@ -122,6 +119,6 @@ class id_vaccines_tweetDataset(datasets.GeneratorBasedBuilder):
                     "subjective": float(subj),
                     "sentiment": float(posneg),
                 }
-        if self.config.schema == "seacrowd.text":
+        if self.config.schema == "seacrowd_text":
             for idx, (ind, norm, posneg) in enumerate(zip(indexes, norms, posnegs)):
                 yield idx, {"id": str(ind), "text": norm, "label": float(posneg)}
