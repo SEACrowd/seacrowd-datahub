efc594d45f40d5c5d6d0f50ec137cb4023760670
Yuze GAO <yuze.gao@outlook.com>
2024-01-11 22:00:35 +0800

    Update stb_ext.py

--- seacrowd/sea_datasets/stb_ext/stb_ext.py
+++ seacrowd/sea_datasets/stb_ext/stb_ext.py
@@ -3,15 +3,18 @@ import io
 import conllu
 import datasets
 
+from seacrowd.utils.common_parser import (load_ud_data,
+                                          load_ud_data_as_seacrowd_kb)
 from seacrowd.utils.configs import SEACrowdConfig
 from seacrowd.utils.constants import (DEFAULT_SEACROWD_VIEW_NAME,
-                                      DEFAULT_SOURCE_VIEW_NAME, Tasks, Licenses)
+                                      DEFAULT_SOURCE_VIEW_NAME, Licenses,
+                                      Tasks)
 
 _DATASETNAME = "stb_ext"
 _SOURCE_VIEW_NAME = DEFAULT_SOURCE_VIEW_NAME
 _UNIFIED_VIEW_NAME = DEFAULT_SEACROWD_VIEW_NAME
 
-_LANGUAGES = ["sg"]
+_LANGUAGES = ["eng"]
 _LOCAL = False
 _CITATION = """\
 @article{10.1145/3321128,
@@ -27,18 +30,18 @@ issn = {2375-4699},
 url = {https://doi.org/10.1145/3321128},
 doi = {10.1145/3321128},
 abstract = {Singlish can be interesting to the computational linguistics community both linguistically, as a major
- low-resource creole based on English, and computationally, for information extraction and sentiment analysis of
- regional social media. In our conference paper, Wang et al. (2017), we investigated part-of-speech (POS) tagging and
- dependency parsing for Singlish by constructing a treebank under the Universal Dependencies scheme and successfully
- used neural stacking models to integrate English syntactic knowledge for boosting Singlish POS tagging and dependency
- parsing, achieving the state-of-the-art accuracies of 89.50% and 84.47% for Singlish POS tagging and dependency,
- respectively. In this work, we substantially extend Wang et al. (2017) by enlarging the Singlish treebank to more
- than triple the size and with much more diversity in topics, as well as further exploring neural multi-task models
- for integrating English syntactic knowledge. Results show that the enlarged treebank has achieved significant
- relative error reduction of 45.8% and 15.5% on the base model, 27% and 10% on the neural multi-task model, and
- 21% and 15% on the neural stacking model for POS tagging and dependency parsing, respectively. Moreover, the
- state-of-the-art Singlish POS tagging and dependency parsing accuracies have been improved to 91.16% and 85.57%,
- respectively. We make our treebanks and models available for further research.},
+low-resource creole based on English, and computationally, for information extraction and sentiment analysis of
+regional social media. In our conference paper, Wang et al. (2017), we investigated part-of-speech (POS) tagging and
+dependency parsing for Singlish by constructing a treebank under the Universal Dependencies scheme and successfully
+used neural stacking models to integrate English syntactic knowledge for boosting Singlish POS tagging and dependency
+parsing, achieving the state-of-the-art accuracies of 89.50% and 84.47% for Singlish POS tagging and dependency,
+respectively. In this work, we substantially extend Wang et al. (2017) by enlarging the Singlish treebank to more
+than triple the size and with much more diversity in topics, as well as further exploring neural multi-task models
+for integrating English syntactic knowledge. Results show that the enlarged treebank has achieved significant
+relative error reduction of 45.8% and 15.5% on the base model, 27% and 10% on the neural multi-task model, and
+21% and 15% on the neural stacking model for POS tagging and dependency parsing, respectively. Moreover, the
+state-of-the-art Singlish POS tagging and dependency parsing accuracies have been improved to 91.16% and 85.57%,
+respectively. We make our treebanks and models available for further research.},
 journal = {ACM Trans. Asian Low-Resour. Lang. Inf. Process.},
 month = {may},
 articleno = {1},
@@ -60,13 +63,11 @@ _HOMEPAGE = "https://github.com/wanghm92/Sing_Par/tree/master/TALLIP19_dataset/t
 _LICENSE = Licenses.MIT.value
 
 _PREFIX = "https://raw.githubusercontent.com/wanghm92/Sing_Par/master/TALLIP19_dataset/treebank/"
-_STB_DATASETS = {
+_URLS = {
     "gold_pos": {
         "train": _PREFIX + "gold_pos/train.ext.conll",
     },
-    "en_ud_autopos": {"train": _PREFIX + "en-ud-autopos/en-ud-train.conllu.autoupos",
-                      "validation": _PREFIX + "en-ud-autopos/en-ud-dev.conllu.ann.auto.epoch24.upos",
-                      "test": _PREFIX + "en-ud-autopos/en-ud-test.conllu.ann.auto.epoch24.upos"},
+    "en_ud_autopos": {"train": _PREFIX + "en-ud-autopos/en-ud-train.conllu.autoupos", "validation": _PREFIX + "en-ud-autopos/en-ud-dev.conllu.ann.auto.epoch24.upos", "test": _PREFIX + "en-ud-autopos/en-ud-test.conllu.ann.auto.epoch24.upos"},
     "auto_pos_multiview": {
         "train": _PREFIX + "auto_pos/multiview/train.autopos.multiview.conll",
         "validation": _PREFIX + "auto_pos/multiview/dev.autopos.multiview.conll",
@@ -78,21 +79,14 @@ _STB_DATASETS = {
         "test": _PREFIX + "auto_pos/stack/test.autopos.stack.conll",
     },
 }
-_POSTAGS = ["ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON",
-                               "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X", "root"]
+_POSTAGS = ["ADJ", "ADP", "ADV", "AUX", "CONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X", "root"]
 _SUPPORTED_TASKS = [Tasks.POS_TAGGING, Tasks.DEPENDENCY_PARSING]
 _SOURCE_VERSION = "1.0.0"
 _SEACROWD_VERSION = "1.0.0"
 
 
 def config_constructor(subset_id, schema, version):
-    return SEACrowdConfig(
-        name=f"{_DATASETNAME}_{subset_id}_{schema}",
-        version=datasets.Version(version),
-        description=_DESCRIPTION,
-        schema=schema,
-        subset_id=subset_id
-    )
+    return SEACrowdConfig(name=f"{_DATASETNAME}_{subset_id}_{schema}", version=datasets.Version(version), description=_DESCRIPTION, schema=schema, subset_id=subset_id)
 
 
 class StbExtDataset(datasets.GeneratorBasedBuilder):
@@ -103,7 +97,7 @@ class StbExtDataset(datasets.GeneratorBasedBuilder):
         config_constructor(subset_id="auto_pos_stack", schema="source", version=_SOURCE_VERSION),
         config_constructor(subset_id="auto_pos_multiview", schema="source", version=_SOURCE_VERSION),
         config_constructor(subset_id="en_ud_autopos", schema="source", version=_SOURCE_VERSION),
-        config_constructor(subset_id="gold_pos", schema="source", version=_SOURCE_VERSION)
+        config_constructor(subset_id="gold_pos", schema="source", version=_SOURCE_VERSION),
     ]
 
     DEFAULT_CONFIG_NAME = f"{_DATASETNAME}_gold_pos_source"
@@ -142,18 +136,15 @@ class StbExtDataset(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         """ "return splitGenerators"""
-        urls = _STB_DATASETS[self.config.name.subset_id]
+        urls = _URLS[self.config.subset_id]
         downloaded_files = dl_manager.download_and_extract(urls)
         splits = []
         if "train" in downloaded_files:
-            splits.append(
-                datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}))
+            splits.append(datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}))
         if "validation" in downloaded_files:
-            splits.append(datasets.SplitGenerator(name=datasets.Split.VALIDATION,
-                                                  gen_kwargs={"filepath": downloaded_files["validation"]}))
+            splits.append(datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["validation"]}))
         if "test" in downloaded_files:
-            splits.append(
-                datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}))
+            splits.append(datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": downloaded_files["test"]}))
         return splits
 
     def _generate_examples(self, filepath):
@@ -167,7 +158,16 @@ class StbExtDataset(datasets.GeneratorBasedBuilder):
             return buffer
 
         with open(filepath, "r", encoding="utf-8") as data_file:
+            # tokenlist1 = list(load_ud_data(filepath))
             tokenlist = list(conllu.parse_incr(process_buffer(data_file)))
+            ataset = load_ud_data_as_seacrowd_kb(filepath, tokenlist)
+
+            # print(tokenlist1[0])
+            print(tokenlist[0])
+            # print(ataset[0])
+            for idx, itm in enumerate(ataset):
+                print(itm)
+                exit(0)
             for idx, sent in enumerate(tokenlist):
                 idx = sent.metadata["sent_id"] if "sent_id" in sent.metadata else idx
                 tokens = [token["form"] for token in sent]
