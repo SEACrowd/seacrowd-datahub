bef5128fde7fc1d8b28809966bb7bd5377892c4d
Yuze GAO <yuze.gao@outlook.com>
2024-01-11 15:25:16 +0800

    Update id_vaccines_tweets.py

--- seacrowd/sea_datasets/id_vaccines_tweets/id_vaccines_tweets.py
+++ seacrowd/sea_datasets/id_vaccines_tweets/id_vaccines_tweets.py
@@ -13,25 +13,14 @@ _UNIFIED_VIEW_NAME = DEFAULT_SEACROWD_VIEW_NAME
 _LANGUAGES = ["ind"]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
 _LOCAL = False
 _CITATION = """\
-@article{Febriyanti_Nursidah_Gustiara_Yulianti_2022, title={ANALISIS SENTIMEN MASYARAKAT INDONESIA TERHADAP PELAKSANAAN
-VAKSIN COVID’19}, volume={13}, url={https://journal.uii.ac.id/khazanah/article/view/21204},
-DOI={10.20885/khazanah.vol13.iss2.art4}, abstractNote={&amp;lt;p&amp;gt;Adanya pandemi Covid’19 di Indonesia
-memberikan dampak besar ke berbagai sektor&amp;lt;br /&amp;gt;kehidupan. Hal ini menyebabkan pemerintah memberikan
-respon cepat untuk mengatasi&amp;lt;br /&amp;gt;kasus ini dengan pembuatan berbagai kebijakan, salah satunya adalah
-kebijakan baru&amp;lt;br /&amp;gt;mengenai vaksinasi yang diwajibkan untuk semua masyarakat di Indonesia.
-Kebijakan&amp;lt;br /&amp;gt;tersebut menimbulkan berbagai macam respon dari masyarakat yang kebanyakan&amp;lt;
-br /&amp;gt;disalurkan melalui media sosial. Adanya respon dari masyarakat tersebut dapat menjadi&amp;lt;br
-/&amp;gt;salah satu acuan untuk melakukan evaluasi terhadap pelaksanaan vaksinasi di Indonesia.&amp;lt;br /&amp;
-gt;Maka dari itu, dengan memanfaatkan data dari media sosial twitter, penelitian ini&amp;lt;br /&amp;gt;bertujuan
- untuk menganalisis respon masyarakat terhadap pelaksanaan vaksinasi&amp;lt;br /&amp;gt;dengan cara mengklasifikasikan
- respon tersebut ke dalam respon positif, negatif, dan&amp;lt;br /&amp;gt;netral. Hasil analisis diperoleh bahwa
- respon masyarakat selama 3 bulan, yaitu pada&amp;lt;br /&amp;gt;bulan Mei dan Juni masih terdapat respon netral
- sedangkan pada bulan Juli keseluruhan&amp;lt;br /&amp;gt;kata memiliki respon negatif seiring dengan kenaikan
- kasus Covid’19.&amp;lt;/p&amp;gt;
-&amp;lt;p&amp;gt;&amp;lt;br /&amp;gt;&amp;lt;strong&amp;gt;Kata Kunci:&amp;lt;/strong&amp;gt; covid’19, vaksinasi,
-pelaksanaan vaksin, kartu/sertifikat vaksin, sentimen&amp;lt;br /&amp;gt;analisis&amp;lt;/p&amp;gt;}, number={2},
-journal={Khazanah: Jurnal Mahasiswa}, author={Febriyanti, Syintya and Nursidah, Dea Ratu and Gustiara, Dela and
-Yulianti, Rika}, year={2022}, month={May} }
+@article{febriyanti2021analisis,
+  title={ANALISIS SENTIMEN MASYARAKAT INDONESIA TERHADAP PELAKSANAAN VAKSIN COVID'19},
+  author={Febriyanti, Syintya and Nursidah, Dea Ratu and Gustiara, Dela and Yulianti, Rika},
+  journal={Khazanah: Jurnal Mahasiswa},
+  volume={13},
+  number={2},
+  year={2021}
+}
 """
 
 _DESCRIPTION = """\
@@ -39,7 +28,7 @@ Dataset containing tweets about COVID-19 vaccines with manually labelled informa
 subjective tweet and their sentiment polarity. Tweets are from 20-27 June 2021 and 15-22 July 2021.
 """
 
-_HOMEPAGE = "	https://github.com/rayendito/id-vaccines-tweets"
+_HOMEPAGE = "https://github.com/rayendito/id-vaccines-tweets"
 
 _LICENSE = Licenses.UNKNOWN.value
 
@@ -100,7 +89,7 @@ class IdVaccinesTweetsDataset(datasets.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager):
         """ "return splitGenerators"""
         downloaded_files = dl_manager.download_and_extract(_URL)
-        return [datasets.SplitGenerator(name="datasets.ALL", gen_kwargs={"filepath": downloaded_files})]
+        return [datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files})]
 
     def _generate_examples(self, filepath):
         data_lines = pandas.read_csv(filepath, skip_blank_lines=True)
