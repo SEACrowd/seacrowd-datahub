3cf80edf45cfdbaa1eaf95e2e5b5602eb3d4c591
Railey Montalan <raileymontalan@Raileys-MacBook-Pro-2023.local>
2024-01-30 12:26:08 +0800

    Implement dataloader for VIVOS

--- seacrowd/sea_datasets/vivos/vivos.py
+++ seacrowd/sea_datasets/vivos/vivos.py
@@ -31,21 +31,31 @@ _CITATION = """\
       Lin, Donghui  and
       Ide, Nancy  and
       Pustejovsky, James",
-    booktitle = "Proceedings of the Third International Workshop on Worldwide Language Service Infrastructure and Second Workshop on Open Infrastructures and Analysis Frameworks for Human Language Technologies ({WLSI}/{OIAF}4{HLT}2016)",
+    booktitle = "Proceedings of the Third International Workshop on Worldwide Language Service
+    Infrastructure and Second Workshop on Open Infrastructures and Analysis Frameworks for
+    Human Language Technologies ({WLSI}/{OIAF}4{HLT}2016)",
     month = dec,
     year = "2016",
     address = "Osaka, Japan",
     publisher = "The COLING 2016 Organizing Committee",
     url = "https://aclanthology.org/W16-5207",
     pages = "51--55",
-    abstract = "In this paper we describe a non-expert setup for Vietnamese speech recognition system using Kaldi toolkit. We collected a speech corpus over fifteen hours from about fifty Vietnamese native speakers and using it to test the feasibility of our setup. The essential linguistic components for the Automatic Speech Recognition (ASR) system was prepared basing on the written form of the language instead of expertise knowledge on linguistic and phonology as commonly seen in rich resource languages like English. The modeling of tones by integrating them into the phoneme and using the phonetic decision tree is also discussed. Experimental results showed this setup for ASR systems does yield competitive results while still have potentials for further improvements.",
+    abstract = "In this paper we describe a non-expert setup for Vietnamese speech recognition
+    system using Kaldi toolkit. We collected a speech corpus over fifteen hours from about fifty
+    Vietnamese native speakers and using it to test the feasibility of our setup. The essential
+    linguistic components for the Automatic Speech Recognition (ASR) system was prepared basing
+    on the written form of the language instead of expertise knowledge on linguistic and phonology
+    as commonly seen in rich resource languages like English. The modeling of tones by integrating
+    them into the phoneme and using the phonetic decision tree is also discussed. Experimental
+    results showed this setup for ASR systems does yield competitive results while still have
+    potentials for further improvements.",
 }
 """
 
 _DATASETNAME = "vivos"
 
 _DESCRIPTION = """\
-VIVOS is a Vietnamese speech corpus consisting of 15 hours of recording speech prepared for 
+VIVOS is a Vietnamese speech corpus consisting of 15 hours of recording speech prepared for
 Automatic Speech Recognition task. This speech corpus is collected by recording speech data
 from more than 50 native Vietnamese volunteers.
 """
@@ -59,9 +69,9 @@ _LICENSE = Licenses.CC_BY_SA_4_0.value
 _LOCAL = False
 
 _URLS = {
-    "audio": "https://huggingface.co/datasets/vivos/blob/main/data/vivos.tar.gz",
-    "train_prompt": "https://huggingface.co/datasets/vivos/blob/main/data/prompts-train.txt.gz",
-    "test_prompt": "https://huggingface.co/datasets/vivos/blob/main/data/prompts-test.txt.gz",
+    "audio": "https://huggingface.co/datasets/vivos/resolve/main/data/vivos.tar.gz",
+    "train_prompt": "https://huggingface.co/datasets/vivos/resolve/main/data/prompts-train.txt.gz",
+    "test_prompt": "https://huggingface.co/datasets/vivos/resolve/main/data/prompts-test.txt.gz",
 }
 
 _SUPPORTED_TASKS = [Tasks.SPEECH_RECOGNITION]
@@ -81,24 +91,22 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     SEACROWD_VERSION = datasets.Version(_SEACROWD_VERSION)
 
-    BUILDER_CONFIGS = (
-        [
-            SEACrowdConfig(
-                name=f"{_DATASETNAME}_source",
-                version=datasets.Version(_SOURCE_VERSION),
-                description=f"{_DATASETNAME} source schema",
-                schema="source",
-                subset_id=f"{_DATASETNAME}",
-            ),
-            SEACrowdConfig(
-                name=f"{_DATASETNAME}_seacrowd_sptext",
-                version=datasets.Version(_SEACROWD_VERSION),
-                description=f"{_DATASETNAME} SEACrowd schema",
-                schema="seacrowd_sptext",
-                subset_id=f"{_DATASETNAME}",
-            ),
-        ]
-    )
+    BUILDER_CONFIGS = [
+        SEACrowdConfig(
+            name=f"{_DATASETNAME}_source",
+            version=datasets.Version(_SOURCE_VERSION),
+            description=f"{_DATASETNAME} source schema",
+            schema="source",
+            subset_id=f"{_DATASETNAME}",
+        ),
+        SEACrowdConfig(
+            name=f"{_DATASETNAME}_seacrowd_sptext",
+            version=datasets.Version(_SEACROWD_VERSION),
+            description=f"{_DATASETNAME} SEACrowd schema",
+            schema="seacrowd_sptext",
+            subset_id=f"{_DATASETNAME}",
+        ),
+    ]
 
     def _info(self) -> datasets.DatasetInfo:
         if self.config.schema == "source":
@@ -111,18 +119,7 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
                 }
             )
         elif self.config.schema == "seacrowd_sptext":
-            features = schemas.speech_text_features()
-            #             {
-            #     "id": datasets.Value("string"),
-            #     "path": datasets.Value("string"),
-            #     "audio": datasets.Audio(sampling_rate=16_000),
-            #     "text": datasets.Value("string"),
-            #     "speaker_id": datasets.Value("string"),
-            #     "metadata": {
-            #         "speaker_age": datasets.Value("int64"),
-            #         "speaker_gender": datasets.Value("string"),
-            #     }
-            # }
+            features = schemas.speech_text_features
 
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
@@ -137,9 +134,9 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
         Returns SplitGenerators.
         """
 
-        audio_path = dl_manager.download(_URLS['audio'])
-        train_prompt_path = dl_manager.download_and_extract(_URLS['train_prompt'])
-        test_prompt_path = dl_manager.download_and_extract(_URLS['test_prompt'])
+        audio_path = dl_manager.download(_URLS["audio"])
+        train_prompt_path = dl_manager.download_and_extract(_URLS["train_prompt"])
+        test_prompt_path = dl_manager.download_and_extract(_URLS["test_prompt"])
 
         return [
             datasets.SplitGenerator(
@@ -148,6 +145,7 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
                     "prompts_path": train_prompt_path,
                     "clips_path": "vivos/train/waves",
                     "audio_files": dl_manager.iter_archive(audio_path),
+                    "split": "train",
                 },
             ),
             datasets.SplitGenerator(
@@ -156,6 +154,7 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
                     "prompts_path": test_prompt_path,
                     "clips_path": "vivos/test/waves",
                     "audio_files": dl_manager.iter_archive(audio_path),
+                    "split": "test",
                 },
             ),
         ]
@@ -164,7 +163,6 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
         """
         Yields examples as (key, example) tuples.
         """
-        idx = 0
         examples = {}
         with open(prompts_path, encoding="utf-8") as f:
             if self.config.schema == "source":
@@ -178,29 +176,29 @@ class VIVOSDataset(datasets.GeneratorBasedBuilder):
                         "sentence": data[1],
                     }
             elif self.config.schema == "seacrowd_sptext":
+                audio_id = 0
                 for row in f:
                     data = row.strip().split(" ", 1)
                     speaker_id = data[0].split("_")[0]
                     audio_path = "/".join([clips_path, speaker_id, data[0] + ".wav"])
                     examples[audio_path] = {
-                        "id": idx,
+                        "id": audio_id,
                         "path": audio_path,
                         "text": data[1],
                         "speaker_id": speaker_id,
                         "metadata": {
                             "speaker_age": None,
                             "speaker_gender": None,
-                        }
+                        },
                     }
+                    audio_id += 1
 
+        idx = 0
         for path, f in audio_files:
             if path.startswith(clips_path):
                 if path in examples:
                     audio = {"path": path, "bytes": f.read()}
-                    yield idx, {
-                        **examples[path], 
-                        "audio": audio
-                    }
+                    yield idx, {**examples[path], "audio": audio}
                     idx += 1
             else:
-                break
+                continue
