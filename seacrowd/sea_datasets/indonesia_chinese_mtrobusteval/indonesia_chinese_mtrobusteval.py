b42f17a6819766a330ebb97e92898d8e3bb64de7
TysonYu <784250886@qq.com>
2024-03-03 16:54:11 +0800

    update code based on the reviewer comments

--- seacrowd/sea_datasets/indonesia_chinese_mtrobusteval/indonesia_chinese_mtrobusteval.py
+++ seacrowd/sea_datasets/indonesia_chinese_mtrobusteval/indonesia_chinese_mtrobusteval.py
@@ -1,34 +1,13 @@
-# coding=utf-8
-# Copyright 2022 The HuggingFace Datasets Authors and the current dataset script contributor.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-The dataset is curated for the purpose of evaluating the robustness of Neural Machine Translation (NMT) towards natural occuring noise
-(typo, slang, code switching, etc.). The dataset is crawled from Twitter, then pre-processed to obtain sentences with noise.
-The dataset consists of a thousand noisy sentences. The dataset is translated into Chinese manually as the benchmark for evaluating the robustness of NMT.
-"""
-import os
 from pathlib import Path
 from typing import Dict, List, Tuple
 
 import datasets
-import pandas as pd
 import jsonlines
+import pandas as pd
 
 from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
-from seacrowd.utils.constants import Tasks, Licenses
+from seacrowd.utils.constants import Licenses, Tasks
 
 _CITATION = """\
 @article{,
@@ -45,7 +24,7 @@ _CITATION = """\
 """
 
 
-_DATASETNAME = "[indonesia_chinese_mtrobusteval]"
+_DATASETNAME = "indonesia_chinese_mtrobusteval"
 
 _DESCRIPTION = """\
 The dataset is curated for the purpose of evaluating the robustness of Neural Machine Translation (NMT) towards natural occuring noise
@@ -55,11 +34,10 @@ The dataset consists of a thousand noisy sentences. The dataset is translated in
 
 _HOMEPAGE = "https://github.com/supryzhu/Indonesia-Chinese-MTRobustEval"
 
-# TODO: Add languages related to this dataset
 _LANGUAGES = ["ind", "cmn"]  # We follow ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
 
 
-_LICENSE = Licenses.MIT.value # example: Licenses.MIT.value, Licenses.CC_BY_NC_SA_4_0.value, Licenses.UNLICENSE.value, Licenses.UNKNOWN.value
+_LICENSE = Licenses.MIT.value  # example: Licenses.MIT.value, Licenses.CC_BY_NC_SA_4_0.value, Licenses.UNLICENSE.value, Licenses.UNKNOWN.value
 
 _LOCAL = False
 
@@ -73,7 +51,8 @@ _SOURCE_VERSION = "1.0.0"
 
 _SEACROWD_VERSION = "1.0.0"
 
-class NewDataset(datasets.GeneratorBasedBuilder):
+
+class IndonesiaChineseMtRobustEval(datasets.GeneratorBasedBuilder):
     """The dataset consists of a thousand noisy sentences. The dataset is translated into Chinese manually as the benchmark for evaluating the robustness of NMT."""
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
@@ -81,22 +60,22 @@ class NewDataset(datasets.GeneratorBasedBuilder):
 
     BUILDER_CONFIGS = [
         SEACrowdConfig(
-            name="indonesia_chinese_mtrobusteval_source",
+            name=f"{_DATASETNAME}_source",
             version=SOURCE_VERSION,
             description="indonesia_chinese_mtrobusteval source schema",
             schema="source",
-            subset_id="indonesia_chinese_mtrobusteval",
+            subset_id=f"{_DATASETNAME}",
         ),
         SEACrowdConfig(
-            name="indonesia_chinese_mtrobusteval_seacrowd_t2t",
+            name=f"{_DATASETNAME}_seacrowd_t2t",
             version=SEACROWD_VERSION,
             description="indonesia_chinese_mtrobusteval SEACrowd schema",
             schema="seacrowd_t2t",
-            subset_id="indonesia_chinese_mtrobusteval",
+            subset_id=f"{_DATASETNAME}",
         ),
     ]
 
-    DEFAULT_CONFIG_NAME = "indonesia_chinese_mtrobusteval_source"
+    DEFAULT_CONFIG_NAME = f"{_DATASETNAME}_source"
 
     def _info(self) -> datasets.DatasetInfo:
 
@@ -130,22 +109,20 @@ class NewDataset(datasets.GeneratorBasedBuilder):
         results = []
         for i, item in enumerate(src):
             results.append({"id": str(i), "src": item, "tgt": tgt[i]})
-        self._write_jsonl(file_path+'.jsonl', results)
+        self._write_jsonl(file_path + ".jsonl", results)
 
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 # Whatever you put in gen_kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": file_path+'.jsonl',
+                    "filepath": file_path + ".jsonl",
                     "split": "train",
                 },
             )
         ]
 
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
-
-    # TODO: change the args of this function to match the keys in `gen_kwargs`. You may add any necessary kwargs.
 
     def _generate_examples(self, filepath: Path, split: str) -> Tuple[int, Dict]:
         if self.config.schema == "source":
@@ -172,11 +149,7 @@ class NewDataset(datasets.GeneratorBasedBuilder):
         with jsonlines.open(filepath, "w") as writer:
             for line in values:
                 writer.write(line)
+
+
 # This template is based on the following template from the datasets package:
 # https://github.com/huggingface/datasets/blob/master/templates/new_dataset_script.py
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
