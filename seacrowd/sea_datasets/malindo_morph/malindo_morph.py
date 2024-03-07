873e409a021b265da48b0dc8375304225f1cce6d
Jennifer Santoso <62987563+jen-santoso@users.noreply.github.com>
2024-03-07 17:16:31 +0900

    Update seacrowd/sea_datasets/malindo_morph/malindo_morph.py

--- seacrowd/sea_datasets/malindo_morph/malindo_morph.py
+++ seacrowd/sea_datasets/malindo_morph/malindo_morph.py
@@ -1,24 +1,3 @@
-# coding=utf-8
-# Copyright 2022 The HuggingFace Datasets Authors and the current dataset script contributor.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-MALINDO Morph is a morphological dictionary for Malay (bahasa Melayu) and Indonesian (bahasa Indonesia) language.
-It contains over 200,000 lines, with each containing an analysis for one (case-sensitive) token.
-Each line is made up of the following six items, separated by tabs: root, surface form, prefix, suffix, circumfix, reduplication.
-"""
-
 from pathlib import Path
 from typing import Dict, List, Tuple
 
