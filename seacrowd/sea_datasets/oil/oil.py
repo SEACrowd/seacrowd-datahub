7f00a44dbb39d1f9363c4f0a324cdcc3886099c2
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-03-13 13:52:12 +0700

    fix: remove seacrowd schema

--- seacrowd/sea_datasets/oil/oil.py
+++ seacrowd/sea_datasets/oil/oil.py
@@ -13,16 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from pathlib import Path
 from typing import Dict, List, Tuple
 
 import datasets
 import pandas as pd
 
-from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
-from seacrowd.utils.constants import TASK_TO_SCHEMA, Licenses, Tasks
+from seacrowd.utils.constants import TASK_TO_SCHEMA, Licenses
 
 _CITATION = """\
 @inproceedings{maxwelll-smith-foley-2023-automated,
@@ -74,7 +72,7 @@ _URLS = {
     _DATASETNAME: {"train": "https://huggingface.co/api/datasets/ZMaxwell-Smith/OIL/parquet/default/train/0.parquet"},
 }
 
-_SUPPORTED_TASKS = [Tasks.SPEECH_RECOGNITION]
+_SUPPORTED_TASKS = []
 _SUPPORTED_SCHEMA_STRINGS = [f"seacrowd_{str(TASK_TO_SCHEMA[task]).lower()}" for task in _SUPPORTED_TASKS]
 
 _SOURCE_VERSION = "1.0.0"
@@ -98,22 +96,6 @@ class OIL(datasets.GeneratorBasedBuilder):
         ),
     ]
 
-    seacrowd_schema_config: list[SEACrowdConfig] = []
-
-    for seacrowd_schema in _SUPPORTED_SCHEMA_STRINGS:
-
-        seacrowd_schema_config.append(
-            SEACrowdConfig(
-                name=f"{_DATASETNAME}_{seacrowd_schema}",
-                version=SEACROWD_VERSION,
-                description=f"{_DATASETNAME} {seacrowd_schema} schema",
-                schema=f"{seacrowd_schema}",
-                subset_id=f"{_DATASETNAME}",
-            )
-        )
-
-    BUILDER_CONFIGS.extend(seacrowd_schema_config)
-
     DEFAULT_CONFIG_NAME = f"{_DATASETNAME}_source"
 
     def _info(self) -> datasets.DatasetInfo:
@@ -125,9 +107,6 @@ class OIL(datasets.GeneratorBasedBuilder):
                     "label": datasets.ClassLabel(num_classes=98),
                 }
             )
-
-        elif self.config.schema == f"seacrowd_{str(TASK_TO_SCHEMA[Tasks.SPEECH_RECOGNITION]).lower()}":
-            features = schemas.speech_text_features
 
         else:
             raise ValueError(f"Invalid config: {self.config.name}")
@@ -162,42 +141,6 @@ class OIL(datasets.GeneratorBasedBuilder):
         if self.config.schema == "source":
 
             df = pd.read_parquet(filepath)
-
-            for index, row in df.iterrows():
-                yield index, row.to_dict()
-
-        elif self.config.schema == f"seacrowd_{str(TASK_TO_SCHEMA[Tasks.SPEECH_RECOGNITION]).lower()}":
-
-            df = pd.read_parquet(filepath)
-
-            base_folder = os.path.dirname(filepath)
-            base_folder = os.path.join(base_folder, _DATASETNAME, split)
-
-            if not os.path.exists(base_folder):
-                os.makedirs(base_folder)
-
-            audio_paths = []
-
-            for _, row in df.iterrows():
-                audio_dict = row["audio"]
-                file_name = audio_dict["path"]
-
-                path = os.path.join(base_folder, file_name)
-
-                audio_dict["path"] = path
-
-                with open(path, "wb") as f:
-                    f.write(audio_dict["bytes"])
-
-                audio_paths.append(path)
-
-            df.rename(columns={"label": "text"}, inplace=True)
-
-            df["path"] = audio_paths
-
-            df["id"] = df.index
-            df = df.assign(speaker_id="").astype({"speaker_id": "str"})
-            df = df.assign(metadata=[{"speaker_age": 0, "speaker_gender": ""}] * len(df)).astype({"metadata": "object"})
 
             for index, row in df.iterrows():
                 yield index, row.to_dict()
