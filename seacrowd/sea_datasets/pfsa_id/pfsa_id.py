ad54e8bde081599c9973f155e8dfc3f34665f3d0
Samuel Cahyawijaya <Samuel.Cahyawijaya@gmail.com>
2024-04-08 17:22:14 +0800

    Update pfsa_id.py
    Update _CITATION

--- seacrowd/sea_datasets/pfsa_id/pfsa_id.py
+++ seacrowd/sea_datasets/pfsa_id/pfsa_id.py
@@ -29,17 +29,31 @@ from seacrowd.utils.constants import Licenses, Tasks
 
 _CITATION = """\
 @article{PURNOMOWP2022,
-	title = {PFSA-ID: an annotated Indonesian corpus and baseline model of public figures statements attributions},
-	journal = {Global Knowledge, Memory and Communication},
-	volume = {ahead-of-print},
-	pages = {ahead-of-print},
-	year = {2022},
-	issn = {2514-9342},
-	doi = {https://doi.org/10.1108/GKMC-04-2022-0091},
-	url = {https://www.emerald.com/insight/content/doi/10.1108/GKMC-04-2022-0091/full/html},
-	author = {Yohanes Sigit {Purnomo W.P.} and Yogan Jaya Kumar and Nur Zareen Zulkarnain},
-	keywords = {Indonesian corpus, Public figures, Statement attribution, News article, Baseline model, Named entity recognition},
-	abstract = {Purpose By far, the corpus for the quotation extraction and quotation attribution tasks in Indonesian is still limited in quantity and depth. This study aims to develop an Indonesian corpus of public figure statements attributions and a baseline model for attribution extraction, so it will contribute to fostering research in information extraction for the Indonesian language. Design/methodology/approach The methodology is divided into corpus development and extraction model development. During corpus development, data were collected and annotated. The development of the extraction model entails feature extraction, the definition of the model architecture, parameter selection and configuration, model training and evaluation, as well as model selection. Findings The Indonesian corpus of public figure statements attribution achieved 90.06% agreement level between the annotator and experts and could serve as a gold standard corpus. Furthermore, the baseline model predicted most labels and achieved 82.026% F-score. Originality/value To the best of the authors’ knowledge, the resulting corpus is the first corpus for attribution of public figures’ statements in the Indonesian language, which makes it a significant step for research on attribution extraction in the language. The resulting corpus and the baseline model can be used as a benchmark for further research. Other researchers could follow the methods presented in this paper to develop a new corpus and baseline model for other languages.}
+  title = {PFSA-ID: an annotated Indonesian corpus and baseline model of public figures statements attributions},
+  journal = {Global Knowledge, Memory and Communication},
+  volume = {ahead-of-print},
+  pages = {ahead-of-print},
+  year = {2022},
+  issn = {2514-9342},
+  doi = {https://doi.org/10.1108/GKMC-04-2022-0091},
+  url = {https://www.emerald.com/insight/content/doi/10.1108/GKMC-04-2022-0091/full/html},
+  author = {Yohanes Sigit {Purnomo W.P.} and Yogan Jaya Kumar and Nur Zareen Zulkarnain},
+  keywords = {Indonesian corpus, Public figures, Statement attribution, News article, Baseline model, Named entity recognition},
+  abstract = {Purpose By far, the corpus for the quotation extraction and quotation attribution tasks in Indonesian is still limited in quantity and depth. This study aims to develop an Indonesian corpus of public figure statements attributions and a baseline model for attribution extraction, so it will contribute to fostering research in information extraction for the Indonesian language. Design/methodology/approach The methodology is divided into corpus development and extraction model development. During corpus development, data were collected and annotated. The development of the extraction model entails feature extraction, the definition of the model architecture, parameter selection and configuration, model training and evaluation, as well as model selection. Findings The Indonesian corpus of public figure statements attribution achieved 90.06% agreement level between the annotator and experts and could serve as a gold standard corpus. Furthermore, the baseline model predicted most labels and achieved 82.026% F-score. Originality/value To the best of the authors’ knowledge, the resulting corpus is the first corpus for attribution of public figures’ statements in the Indonesian language, which makes it a significant step for research on attribution extraction in the language. The resulting corpus and the baseline model can be used as a benchmark for further research. Other researchers could follow the methods presented in this paper to develop a new corpus and baseline model for other languages.}
+}
+
+@article{PurnomoWP2024,
+  title = {Extraction and attribution of public figures statements for journalism in Indonesia using deep learning},
+  volume = {289},
+  ISSN = {0950-7051},
+  url = {http://dx.doi.org/10.1016/j.knosys.2024.111558},
+  DOI = {10.1016/j.knosys.2024.111558},
+  journal = {Knowledge-Based Systems},
+  publisher = {Elsevier BV},
+  author = {Purnomo W.P.,  Yohanes Sigit and Kumar,  Yogan Jaya and Zulkarnain,  Nur Zareen and Raza,  Basit},
+  year = {2024},
+  month = apr,
+  pages = {111558}
 }
 """
 
