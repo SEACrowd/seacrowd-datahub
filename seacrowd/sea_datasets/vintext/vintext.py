fe9f1385ddbb76b01788ec5eaab0d862022ea1c2
patrickamadeus <patrickai0309@gmail.com>
2024-04-16 21:04:13 +0700

    feat: add VinText dataloader

--- seacrowd/sea_datasets/vintext/vintext.py
+++ seacrowd/sea_datasets/vintext/vintext.py
@@ -14,18 +14,20 @@
 # limitations under the License.
 
 """
-Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols. This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters. The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images).
+Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols.
+This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters.
+The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images).
 """
 import os
-import requests
 from pathlib import Path
 from typing import Dict, List, Tuple
 
 import datasets
+import gdown
 
 from seacrowd.utils import schemas
 from seacrowd.utils.configs import SEACrowdConfig
-from seacrowd.utils.constants import Tasks, Licenses
+from seacrowd.utils.constants import Licenses, Tasks
 
 _CITATION = """\
 @inproceedings{m_Nguyen-etal-CVPR21,
@@ -39,7 +41,9 @@ _CITATION = """\
 _DATASETNAME = "vintext"
 
 _DESCRIPTION = """\
-Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols. This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters. The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images).
+Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols.
+This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters.
+The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images).
 """
 
 _HOMEPAGE = "https://github.com/VinAIResearch/dict-guided"
@@ -51,7 +55,6 @@ _LICENSE = Licenses.AGPL_3_0.value
 _LOCAL = False
 
 _GDRIVE_ID = "1UUQhNvzgpZy7zXBFQp0Qox-BBjunZ0ml"
-_URLS = "https://drive.google.com/uc?export=download&confirm=yTib&id=1UUQhNvzgpZy7zXBFQp0Qox-BBjunZ0ml"
 
 _SUPPORTED_TASKS = [Tasks.OPTICAL_CHARACTER_RECOGNITION]
 
@@ -59,8 +62,11 @@ _SOURCE_VERSION = "1.0.0"
 
 _SEACROWD_VERSION = "1.0.0"
 
+
 class VintextDataset(datasets.GeneratorBasedBuilder):
-    """Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols. This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters. The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images)."""
+    """Vintext is a challenging scene text dataset for Vietnamese, where some characters are equivocal in the visual form due to accent symbols.
+    This dataset contains 2000 fully annotated images with 56,084 text instances. Each text instance is delineated by a quadrilateral bounding box and associated with the ground truth sequence of characters.
+    The dataset is randomly split into three subsets for training (1,200 images), validation (300 images), and testing (500 images)."""
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     SEACROWD_VERSION = datasets.Version(_SEACROWD_VERSION)
@@ -87,10 +93,30 @@ class VintextDataset(datasets.GeneratorBasedBuilder):
     def _info(self) -> datasets.DatasetInfo:
 
         if self.config.schema == "source":
-            features = datasets.Features({
-                "id": datasets.Value("string"),
-                "image_path": datasets.Value("string"),
-                "annotations": datasets.Sequence({
+            features = datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "image_path": datasets.Value("string"),
+                    "annotations": datasets.Sequence(
+                        {
+                            "x1": datasets.Value("int32"),
+                            "y1": datasets.Value("int32"),
+                            "x2": datasets.Value("int32"),
+                            "y2": datasets.Value("int32"),
+                            "x3": datasets.Value("int32"),
+                            "y3": datasets.Value("int32"),
+                            "x4": datasets.Value("int32"),
+                            "y4": datasets.Value("int32"),
+                            "transcript": datasets.Value("string"),
+                        }
+                    ),
+                }
+            )
+
+        elif self.config.schema == "seacrowd_imtext":
+            features = schemas.image_text_features()
+            features["metadata"]["meta"]["annotations"] = datasets.Sequence(
+                {
                     "x1": datasets.Value("int32"),
                     "y1": datasets.Value("int32"),
                     "x2": datasets.Value("int32"),
@@ -100,12 +126,8 @@ class VintextDataset(datasets.GeneratorBasedBuilder):
                     "x4": datasets.Value("int32"),
                     "y4": datasets.Value("int32"),
                     "transcript": datasets.Value("string"),
-                })
-            })
-
-        # For example seacrowd_kb, seacrowd_t2t
-        elif self.config.schema == "seacrowd_imtext":
-            features = schemas.image_text_features
+                }
+            )
 
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
@@ -115,111 +137,93 @@ class VintextDataset(datasets.GeneratorBasedBuilder):
             citation=_CITATION,
         )
 
-    def _gdrive_large_file_get_url(self, base_url: str, base_id: str):
-        """Get the final download URL for large files on Google Drive."""
-        # Based on https://stackoverflow.com/a/39225039
-        session = requests.Session()
-        response = session.get(base_url, stream=True)
-        token = None
-
-        print(response.cookies.items())
-        for key, value in response.cookies.items():
-            if key.startswith("download_warning"):
-                token = value
-        print("GENERATED TOKEN: ", token)
-        if token:
-            params = {"id": base_id, "confirm": token}
-            response = session.get(base_url, params=params, stream=True)
-        return response.url
-
     def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
         """Returns SplitGenerators."""
 
-        direct_url = self._gdrive_large_file_get_url(_URLS, _GDRIVE_ID)
+        zip_filepath = os.path.join(os.path.dirname(__file__), "vietnamese_original.zip")
+        if not os.path.exists(zip_filepath):
+            gdown.download(id=_GDRIVE_ID, output=zip_filepath)
 
-        print(f"Downloading and extracting {direct_url}")
-        data_dir = dl_manager.download_and_extract(direct_url)
-
-        print(data_dir)
+        data_dir = dl_manager.extract(zip_filepath)
 
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 gen_kwargs={
-                    "images": os.path.join(data_dir, "train_images"),
-                    "labels": os.path.join(data_dir, "labels"),
+                    "imagepath": os.path.join(data_dir, "vietnamese/train_images"),
+                    "labelpath": os.path.join(data_dir, "vietnamese/labels"),
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 gen_kwargs={
-                    "images": os.path.join(data_dir, "test_images"),
-                    "labels": os.path.join(data_dir, "labels"),
+                    "imagepath": os.path.join(data_dir, "vietnamese/test_image"),
+                    "labelpath": os.path.join(data_dir, "vietnamese/labels"),
                 },
-            )
+            ),
         ]
 
-    def _generate_examples(self, images: Path, labels: Path) -> Tuple[int, Dict]:
+    def _generate_examples(self, imagepath: Path, labelpath: Path) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
-        
+
         df_list = []
-        for image_file in images.iterdir():
-            image_id = image_file.stem
-            label_file = labels / f"gt_{image_id}.txt"
+
+        for image in os.listdir(imagepath):
+            image_id = int(image.split(".")[0][2:])
+            label_file = os.path.join(labelpath, f"gt_{image_id}.txt")
             with open(label_file, "r") as f:
                 label = f.read().strip()
-            df_list.append({"id": image_id, "image_path": str(image_file), "label": label})
+            df_list.append({"id": image_id, "image_path": os.path.join(imagepath, image), "label": label})
 
         if self.config.schema == "source":
             for i, row in enumerate(df_list):
+                labels = [label.split(",") for label in row["label"].split("\n")]
 
                 yield i, {
                     "id": row["id"],
                     "image_path": row["image_path"],
                     "annotations": [
                         {
-                            "x1": x1,
-                            "y1": y1,
-                            "x2": x2,
-                            "y2": y2,
-                            "x3": x3,
-                            "y3": y3,
-                            "x4": x4,
-                            "y4": y4,
-                            "transcript": transcript,
+                            "x1": label[0],
+                            "y1": label[1],
+                            "x2": label[2],
+                            "y2": label[3],
+                            "x3": label[4],
+                            "y3": label[5],
+                            "x4": label[6],
+                            "y4": label[7],
+                            "transcript": label[8],
                         }
-                        for x1, y1, x2, y2, x3, y3, x4, y4, transcript in row["label"].split("\n")
-                    ]
+                        for label in labels
+                    ],
                 }
 
         elif self.config.schema == "seacrowd_imtext":
             for i, row in enumerate(df_list):
+                labels = [label.split(",") for label in row["label"].split("\n")]
 
                 yield i, {
                     "id": row["id"],
-                    "image_path": row["image_path"],
-                    "annotations": [
-                        {
-                            "x1": x1,
-                            "y1": y1,
-                            "x2": x2,
-                            "y2": y2,
-                            "x3": x3,
-                            "y3": y3,
-                            "x4": x4,
-                            "y4": y4,
-                            "transcript": transcript,
-                        }
-                        for x1, y1, x2, y2, x3, y3, x4, y4, transcript in row["label"].split("\n")
-                    ]
+                    "image_paths": [row["image_path"]],
+                    "texts": "",
+                    "metadata": {
+                        "context": "",
+                        "labels": [],
+                        "meta": {
+                            "annotations": [
+                                {
+                                    "x1": label[0],
+                                    "y1": label[1],
+                                    "x2": label[2],
+                                    "y2": label[3],
+                                    "x3": label[4],
+                                    "y3": label[5],
+                                    "x4": label[6],
+                                    "y4": label[7],
+                                    "transcript": label[8],
+                                }
+                                for label in labels
+                            ]
+                        },
+                    },
                 }
-
-
-# This template is based on the following template from the datasets package:
-# https://github.com/huggingface/datasets/blob/master/templates/new_dataset_script.py
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
--- seacrowd/utils/schemas/image_text.py
+++ seacrowd/utils/schemas/image_text.py
@@ -12,7 +12,16 @@ def features(label_names = ["Yes", "No"]):
             "metadata": {
                 "context": datasets.Value("string"),
                 "labels": datasets.Sequence(datasets.ClassLabel(names=label_names)),
-            }
+                
+                # the schema of 'meta' aren't specified either to allow some flexibility
+                "meta": {}
+
+                # notes on how to use this field of 'meta'
+                # you can choose two of options:
+                # 1. defining as empty dict if you don't think it's usable in `_generate_examples`, or
+                # 2. defining meta as dict of key with intended colname meta and its val with dataset.Features class
+                #    in `_info` Dataloader method then populate it with the values in `_general_examples` Dataloader method
+            },
         }
     )
 
