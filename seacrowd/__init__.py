3f8c571e992547fe58b85f319eac621fa1bceac0
Holy Lovenia <holy.lovenia@gmail.com>
2024-04-21 08:56:39 +0000

    Add the latest version

--- seacrowd/__init__.py
+++ seacrowd/__init__.py
@@ -2,4 +2,4 @@ from .utils.constants import Tasks
 from .config_helper import list_datasets, load_dataset, load_datasets, list_benchmarks, load_benchmark
 from .config_helper import SEACrowdMetadata, SEACrowdConfigHelper, SEACrowdMetadataHelper
 
-__version__ = "0.0.10"
+__version__ = "0.0.12"
