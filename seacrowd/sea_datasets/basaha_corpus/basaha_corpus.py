e9946a2b521119178b3c922687a4f5808ec1b9a4
Ravi <muhammadravi251001@gmail.com>
2024-04-28 15:17:26 +0700

    remove template header and simplify additional description

--- seacrowd/sea_datasets/basaha_corpus/basaha_corpus.py
+++ seacrowd/sea_datasets/basaha_corpus/basaha_corpus.py
@@ -1,27 +1,3 @@
-# coding=utf-8
-# Copyright 2022 The HuggingFace Datasets Authors and the current dataset script contributor.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-BasahaCorpus contains short stories in four Central Philippine languages \
-    (Minasbate, Rinconada, Kinaray-a, and Hiligaynon) for low-resource \
-    readability assessment. Each dataset per language contains stories \
-    distributed over the first three grade levels (L1, L2, and L3) in \
-    the Philippine education context. The grade levels of the dataset \
-    have been provided by an expert from Let's Read Asia.
-"""
-
 from pathlib import Path
 from typing import Dict, List, Tuple
 
@@ -91,12 +67,12 @@ _SEACROWD_VERSION = "1.0.0"
 
 class BasahaCorpusDataset(datasets.GeneratorBasedBuilder):
     """
-    BasahaCorpus contains short stories in four Central Philippine languages \
-        (Minasbate, Rinconada, Kinaray-a, and Hiligaynon) for low-resource \
-        readability assessment. Each dataset per language contains stories \
-        distributed over the first three grade levels (L1, L2, and L3) in \
-        the Philippine education context. The grade levels of the dataset \
-        have been provided by an expert from Let's Read Asia.
+    BasahaCorpus comprises short stories in four Central Philippine
+    languages (Minasbate, Rinconada, Kinaray-a, and Hiligaynon)
+    for low-resource readability assessment. Each language dataset
+    includes stories from the first three grade levels (L1, L2, and L3)
+    in the Philippine education context, as classified by an expert
+    from Let's Read Asia.
     """
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
