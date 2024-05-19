f02af03b51b496d3a48813de270b699885a046f8
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2024-05-19 15:50:28 +0700

    nitpick yield None string
    Co-authored-by: Akhdan Fadhilah <akhdan.fadh@gmail.com>

--- seacrowd/sea_datasets/vintext/vintext.py
+++ seacrowd/sea_datasets/vintext/vintext.py
@@ -195,7 +195,7 @@ class VintextDataset(datasets.GeneratorBasedBuilder):
                 yield i, {
                     "id": row["id"],
                     "image_paths": [row["image_path"]],
-                    "texts": "",
+                    "texts": None,
                     "metadata": {
                         "context": "",
                         "labels": [],
