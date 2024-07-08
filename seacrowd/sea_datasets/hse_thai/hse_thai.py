18bb7f72ffae2f4b2ce7e09e860254a3bb7defe6
Maria Khelli <13520115@std.stei.itb.ac.id>
2024-07-08 17:47:18 +0900

    [Fix] Changes based on reviews

--- seacrowd/sea_datasets/hse_thai/hse_thai.py
+++ seacrowd/sea_datasets/hse_thai/hse_thai.py
@@ -1,5 +1,5 @@
-import csv
 import os
+import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import Dict, List, Tuple
 
@@ -10,31 +10,37 @@ from seacrowd.utils.configs import SEACrowdConfig
 from seacrowd.utils.constants import Licenses, Tasks
 
 _CITATION = """\
-    @misc{rtatman2017hse_thai,
-        author = {Rachel Tatman},
-        title = {HSE Thai Corpus},
-        howpublished = {\\url{https://www.kaggle.com/datasets/rtatman/hse-thai-corpus}},
-        note = {Accessed: 2023-11-22}
-    }
+@misc{HSEThaiCorpus,
+  title        = {HSE Thai Corpus},
+  author       = {Conneau, Alexis and Rinott, Ruty and Lample, Guillaume and Williams, Adina and Bowman, Samuel R. and Schwenk, Holger and Stoyanov, Veselin},
+  organization = {HSE School of Linguistics},
+  year         = {2024},
+  howpublished = {\\url{http://web-corpora.net/ThaiCorpus/search}},
+  note         = {Accessed on 2024-05-15}
+}
 """
 
 _DATASETNAME = "hse_thai"
 
 _DESCRIPTION = """\
-HSE Thai Corpus is a corpus of modern texts written in Thai language. The texts, containing in whole 50 million tokens, were collected from various Thai websites (mostly news websites). To make it easier for non-Thai-speakers to comprehend and use texts in the corpus the researchers decided to separate words in each sentence with spaces. The data for the corpus was collected by means of Scrapy. To tokenize texts the Pythai module was used. The text in this dataset is encoded in UTF-8. This dataset contains text from two sources: Wikipedia and thaigov.go.th. The former is licensed under a standard Wikipedia license, and the latter under an Open Government License for Thailand.
+HSE Thai Corpus is a corpus of modern texts written in Thai language. The texts, containing in whole 50 million tokens,
+were collected from various Thai websites (mostly news websites). To make it easier for non-Thai-speakers to comprehend and use texts in the corpus the researchers decided to separate words in each sentence with spaces.
 
-Before running the dataset, please make sure your CLI can run Kaggle API. Guide for installing: https://github.com/Kaggle/kaggle-api/blob/main/docs/README.md
+The data for the corpus was collected by means of Scrapy. To tokenize texts the Pythai module was used. The text in this dataset is encoded in UTF-8.
+This dataset contains text from two sources: Wikipedia and thaigov.go.th.
+
+The former is licensed under a standard Wikipedia license, and the latter under an Open Government License for Thailand.
 """
 
-_HOMEPAGE = "https://www.kaggle.com/datasets/rtatman/hse-thai-corpus"
+_HOMEPAGE = "http://web-corpora.net/ThaiCorpus/search/"
 
 _LANGUAGES = ["tha"]
 
-_LICENSE = Licenses.APACHE_2_0.value
+_LICENSE = Licenses.OTHERS.value
 
 _LOCAL = False
 
-_URLS = "rtatman/hse-thai-corpus"
+_URLS = "https://github.com/khelli07/hse-thai-for-seacrowd/raw/master/texts_tagged.zip"
 
 _SUPPORTED_TASKS = [Tasks.LANGUAGE_IDENTIFICATION]
 
@@ -44,7 +50,7 @@ _SEACROWD_VERSION = "1.0.0"
 
 
 class HSEThaiDataset(datasets.GeneratorBasedBuilder):
-    """Modern Thai corpus taken from https://www.kaggle.com/datasets/rtatman/hse-thai-corpus"""
+    """Modern Thai corpus taken from http://web-corpora.net/ThaiCorpus/search/"""
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     SEACROWD_VERSION = datasets.Version(_SEACROWD_VERSION)
@@ -70,16 +76,7 @@ class HSEThaiDataset(datasets.GeneratorBasedBuilder):
     DEFAULT_CONFIG_NAME = f"{_DATASETNAME}_source"
 
     def _info(self) -> datasets.DatasetInfo:
-        if self.config.schema == "source":
-            features = datasets.Features(
-                {
-                    "article": datasets.Value("string"),
-                    "text": datasets.Value("string"),
-                }
-            )
-
-        elif self.config.schema == f"seacrowd_{self.SEACROWD_SCHEMA_NAME}":
-            features = schemas.text_features(_LANGUAGES)
+        features = schemas.text_features(_LANGUAGES)
 
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
@@ -89,12 +86,13 @@ class HSEThaiDataset(datasets.GeneratorBasedBuilder):
             citation=_CITATION,
         )
 
-    def _split_generators(self, dl_manager: datasets.DownloadManager) -> List[datasets.SplitGenerator]:
+    def _split_generators(
+        self, dl_manager: datasets.DownloadManager
+    ) -> List[datasets.SplitGenerator]:
         """Returns SplitGenerators."""
-        kaggle_path = _URLS
-        os.system(f"kaggle datasets download {kaggle_path}")
+        data_dir = dl_manager.download_and_extract(_URLS)
+        data_dir = os.path.join(data_dir, "texts_tagged_200")
 
-        data_dir = dl_manager.extract(f"{os.getcwd()}/hse-thai-corpus.zip")
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
@@ -108,32 +106,22 @@ class HSEThaiDataset(datasets.GeneratorBasedBuilder):
         """Yields examples as (key, example) tuples."""
 
         base_path = filepath
-        if self.config.schema == "source":
-            i = -1
-            added_text = set()
-            for filepath in os.listdir(base_path):
-                with open(f"{base_path}/{filepath}", mode="r", encoding="utf-8") as file:
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        i += 1
-                        if row["text"] in added_text:
-                            continue
-                        added_text.add(row["text"])
-                        yield i, {"article": row["article"], "text": row["text"]}
+        folders = os.listdir(base_path)
 
-        elif self.config.schema == f"seacrowd_{self.SEACROWD_SCHEMA_NAME}":
-            i = -1
-            added_text = set()
-            for filepath in os.listdir(base_path):
-                with open(f"{base_path}/{filepath}", mode="r", encoding="utf-8") as file:
-                    reader = csv.DictReader(file)
-                    for row in reader:
-                        i += 1
-                        if row["text"] in added_text:
-                            continue
-                        added_text.add(row["text"])
-                        yield i, {
-                            "id": str(i),
-                            "text": row["text"],
+        id_ = 0
+        for folder in folders:
+            files = os.listdir(os.path.join(base_path, folder))
+            for file in files[3:]:
+                filepath = os.path.join(base_path, folder, file)
+                if filepath.endswith(".xml"):
+                    root = ET.parse(filepath).getroot()
+                    sentences = root.findall(".//se")
+                    for sentence in sentences:
+                        words = sentence.findall("w")
+                        article = " ".join([word.text for word in words])
+                        id_ += 1
+                        yield id_, {
+                            "id": str(id_),
+                            "text": article,
                             "label": "tha",
                         }
